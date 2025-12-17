8bffc37211da87a5d06346f887fbd839c3cdd6a3
Namatullah <namatullah.shahbazi444@gmail.com>
2025-12-17 09:40:45 +0430

    pages route are protected

--- client/src/app/(tasks)/layout.tsx
+++ client/src/app/(tasks)/layout.tsx
@@ -1,13 +1,34 @@
 "use client";
+
 import MainDrawer from "@/components/layout/MainDrawer";
 import MainDrawerHeader from "@/components/layout/MainDrawerHeader";
+import { useAuth } from "@/hooks/AuthContext";
 import { Box } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function PagesLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const { isAuthenticated, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      router.push("/auth");
+    }
+  }, [loading, isAuthenticated, router]);
+
+  if (loading) {
+    return null; // or a spinner
+  }
+
+  if (!isAuthenticated) {
+    return null; // prevent UI flash
+  }
+
   return (
     <Box sx={{ display: "flex" }}>
       <MainDrawer />
